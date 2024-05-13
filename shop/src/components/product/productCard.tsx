7544173ca828e42e595df4624c66c3138e9f4abc
rsKaurav98 <kauuravrohit98@gmail.com>
2024-05-13 17:12:36 +0530

    latest commit

--- shop/src/components/product/productCard.tsx
+++ shop/src/components/product/productCard.tsx
@@ -34,7 +34,7 @@ export default function ProductCard({ product }: { product: Product }) {
   const isFreeItem = isFree(product?.sale_price ?? product?.price);
   return (
     
-      <div className="flex h-56   items-start justify-between pt-3.5 dark:bg-dark-200 dark:text-brand-dark p-5 rounded-l border-transparent bg-white  dark:bg-dark-200 shadow-md hover:shadow-xl transition-shadow duration-300">
+      <div className="flex h-56   items-start  pt-3.5 dark:bg-dark-200 dark:text-brand-dark p-5 rounded-l border-transparent bg-white  dark:bg-dark-200 shadow-md hover:shadow-xl transition-shadow duration-300">
         {/* <div className="relative flex h-36 w-36 flex-shrink-0 overflow-hidden 4xl:h-9 4xl:w-9">
           <Image
             alt={shop?.name}
@@ -47,7 +47,7 @@ export default function ProductCard({ product }: { product: Product }) {
               33vw"
           />
         </div> */}
-        <div className=" flex-col   items-center justify-center ltr:pl-2.5 rtl:ml-auto rtl:pr-2.5 rtl:text-right">
+        <div className=" flex-col  w-2/5 items-center justify-center ltr:pl-2.5 rtl:ml-auto rtl:pr-2.5 rtl:text-right">
           <div className='mb-2 flex flex-shrink-0  items-center gap-4 p-1'>
             <span className='flex  text-xs items-center rounded-2xl bg-light-300 px-3 py-1 text-13px font-semibold capitalize text-sm text-brand dark:bg-dark-300 dark:text-brand-dark'>
              <CheckIconWithBg className=' w-4 h-4 mr-1'/>
@@ -71,7 +71,7 @@ export default function ProductCard({ product }: { product: Product }) {
             {shop?.name}
           </AnchorLink>
           <div className='flex  flex-col mt-2 ml-0 flex flex-shrink-0   p-1'>
-            <span className='mt-2 w-2/5 bg-light-300 px-3 py-1 text-13px font-semibold capitalize text-sm text-brand dark:bg-dark-300 dark:text-brand-dark'>
+            <span className='mt-2 w-2/5 bg-light-300 rounded-2xl px-3 py-1 text-13px font-semibold capitalize text-sm text-brand dark:bg-dark-300 dark:text-brand-dark'>
              News,Media & Updates
             </span>
            <span className='flex  mt-2 text-lg text-[#05AAFB] '>
@@ -80,20 +80,20 @@ export default function ProductCard({ product }: { product: Product }) {
             </span>
           </div>
         </div>
-        <div className=' '>
+        <div className=' w-2/5 '>
           <h1>Test Goes here</h1>
         </div>
 
-        <div className="flex  relative flex-shrink-0 flex-col items-center  pl-6">
-          <span className="mt-2 rounded-l bg-light-200 px-8 py-3 text-13px font-semibold uppercase text-lg text-brand dark:bg-dark-300 dark:text-brand-dark">
+        <div className="flex w-1/5   flex-col items-center  pl-6">
+          <span className="mt-2 rounded-l bg-light-00 px-8 py-3 text-4xl font-bold uppercase text-lg text-brand dark:bg-dark-300 dark:text-brand-dark">
             {isFreeItem ? t('text-free') : price}
           </span>
-          {!isFreeItem && basePrice && (
+          {/* {!isFreeItem && basePrice && (
             <del className="px-1 text-20px font-medium text-dark-900 dark:text-dark-700">
               {basePrice}
             </del>
-          )}
-          <button className=" flex mt-16  rounded-l bg-light-500 px-8 py-3 text-lg font-semibold text-white text-brand bg-[#50C878] dark:text-white  ">
+          )} */}
+          <button className=" flex mt-8  rounded-lg bg-light-500 px-16 py-3 text-xl font-semibold text-white text-brand bg-[#50C878] dark:text-white  ">
             Buy
           </button>
         </div>
