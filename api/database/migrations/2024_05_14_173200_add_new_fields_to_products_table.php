da74a96416c4d8534d83c8b53aa5145503dea0d4
himanshubhadoria2727 <hbhadoria78@gmail.com>
2024-05-15 11:51:38 +0530

    migrated the new table columns and updated the product schema

---
+++ api/database/migrations/2024_05_14_173200_add_new_fields_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+        $table->double('domain_authority')->after('domain_name');
+        $table->double('domain_rating')->after('domain_authority');
+        $table->double('organic_traffic')->after('domain_rating');
+        $table->double('spam_score')->after('organic_traffic');
+        $table->string('languages')->after('spam_score');
+        $table->string('countries')->after('languages');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
